92c66814dc863dbe2fe1f9415e1183fab6d17743
iqbalmln <iqbalmaulana15784@gmail.com>
2022-05-05 00:02:54 +0700

    feat: add new component for status order

---
+++ src/components/commons/Status.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="isProses" class="flex gap-2 items-center py-2 px-4 rounded">
+    <Icon icon="bxs-check-circle" class="text-2xl" />
+    <span class="text-sm">Selesai</span>
+  </div>
+</template>
+
+<script>
+
+import Icon from '../base/Icon.vue'
+
+export default {
+  name: 'Status',
+  
+  components: {
+    Icon
+  },
+
+  data () {
+    return {
+      pending: 'berhasil'
+    }
+  },
+
+  computed: {
+    isProses() {
+      return this.pending == 'pending' ? 'text-yellow-500' : 'text-sr-green bg-sr-bg-success'
+    }
+  }
+}
+</script>
