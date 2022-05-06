d7c42050dc3599caa646c516594e1011787191e1
iqbalmln <iqbalmaulana15784@gmail.com>
2022-05-07 03:24:01 +0700

    refactor: add library axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 // Import CSS Global
 import './index.css'
 
+// Import Axios Global
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+
 import router from './router'
 
 Vue.config.productionTip = false
