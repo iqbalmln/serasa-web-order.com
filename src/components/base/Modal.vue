09a45cbb073c8cb0f5072a7296c5608d052a95ef
iqbalmln <iqbalmaulana15784@gmail.com>
2022-05-21 17:56:46 +0700

    feat: init modal

---
+++ src/components/base/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="flex items-center justify-center w-full text-white">
+    <Icon icon="bxs-basket" />
+    Tambahkan item - {{priceItem}}
+  </button>
+</template>
+
+<script>
+
+import Icon from '../base/Icon.vue'
+export default {
+  name: 'Modal',
+  components: {
+    Icon
+  },
+  props: {
+    priceItem: {
+      type: Number,
+      default: null
+    }
+  }
+}
+</script>
