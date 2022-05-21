6df4ca4a3b1fb56d309e14905b52e95914621223
iqbalmln <iqbalmaulana15784@gmail.com>
2022-05-21 17:48:18 +0700

    feat: update card component

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -7,21 +7,18 @@
         alt=""
       >
       <div class="flex-1 text-left">
-        <p class="text-sr-primary text-sm font-semibold">{{ foodMenu }}</p>
+        <p class="text-sr-primary text-sm font-semibold">{{ name }}</p>
         <p class="pt-4 pb-2 text-xs text-sr-secondary">
-          {{ foodDescription }}
+          {{ description }}
         </p>
-        <p class="text-sr-primary text-sm font-semibold">{{ foodPrice }}</p>
+        <p class="text-sr-primary text-sm font-semibold">{{ price }}</p>
       </div>
     </div>
     <Input type="textarea" placeholder="Catatan..." />
     <div class="flex items-center justify-around text-xl">
-      <Icon @click="decrement" icon="bx-minus-circle" class="text-sr-green" />
-      <p class="text-sr-secondary">{{ total }}</p>
-      <Icon @click="increment" icon="bx-plus-circle" class="text-sr-green" />
-    </div>
-    <div>
-
+      <Icon @click="$emit('decrement')" icon="bx-minus-circle" class="text-sr-green" />
+      <p class="text-sr-secondary">{{ quantity }}</p>
+      <Icon @click="$emit('increment')" icon="bx-plus-circle" class="text-sr-green" />
     </div>
   </div>
 </template>
@@ -38,37 +35,26 @@ export default {
     Input
   },
 
-  data () {
-    return {
-      total: 0
-    }
-  },
-
-  methods: {
-    increment() {
-      this.total++  
-    },
-    decrement() {
-      this.total--
-    }
-  },
-
   props: {
     src: {
       type: String,
       default: 'https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
     },
-    foodMenu: {
+    name: {
       type: String,
       required: true,
     },
-    foodDescription: {
+    description: {
       type: String,
       default: ''
     },
-    foodPrice: {
-      type: String,
-      default: 'Gratis'
+    price: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      default: 0
     }
   }
 }
